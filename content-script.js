305b8c91e44b1f463a01f3d4a1a8557aed2956fa
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-10-20 10:48:11 +0200

    initial commit

---
+++ content-script.js
@@ -0,0 +1,13 @@
+const readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+
+        const radioButtons = Array.from(document.querySelectorAll("span.radio-content")).filter(r => r.innerHTML === "Maven Project");
+
+        // "Maven Project" radio button exists
+        if (radioButtons.length === 1) {
+            console.log("Selecting 'Maven Project'");
+            radioButtons[0].click();
+        }
+    }
+}, 10);

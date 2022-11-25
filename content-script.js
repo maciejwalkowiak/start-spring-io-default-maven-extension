3598be077acac5896e8a97ec8f10d5d4e7f5f159
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-11-25 14:08:22 +0100

    Fix selecting Maven after changes at start.spring.io website.

--- content-script.js
+++ content-script.js
@@ -2,7 +2,7 @@ const readyStateCheckInterval = setInterval(function () {
     if (document.readyState === "complete") {
         clearInterval(readyStateCheckInterval);
 
-        const radioButtons = Array.from(document.querySelectorAll("span.radio-content")).filter(r => r.innerHTML === "Maven Project");
+        const radioButtons = Array.from(document.querySelectorAll("span.radio-content")).filter(r => r.innerHTML === "Maven");
 
         // "Maven Project" radio button exists
         if (radioButtons.length === 1) {
